f9609332973519a4406850ff3d02e0c891995c03
Dexsam07 <txgaminglover2@gmail.com>
2026-01-23 23:03:58 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dex Bot',
+  author: '‎',
+  botName: "Dex Bot",
+  botOwner: 'Dex Shyam Chaudhari', // Your name
+  ownerNumber: '917384287404', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
