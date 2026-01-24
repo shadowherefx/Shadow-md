44914fa67f249ef8919c80b5bd7db88c50b48469
Dexsam07 <txgaminglover2@gmail.com>
2026-01-24 18:44:54 +0530

    Add files via upload

--- commands/autotyping.js
+++ commands/autotyping.js
@@ -1,5 +1,5 @@
 /**
- * Knight Bot - A WhatsApp Bot
+ * Dex Bot - A WhatsApp Bot
  * Autotyping Command - Shows fake typing status
  */
 
@@ -31,8 +31,8 @@ async function autotypingCommand(sock, chatId, message) {
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '120363406449026172@newsletter',
+                        newsletterName: 'DexBot MD',
                         serverMessageId: -1
                     }
                 }
@@ -62,8 +62,8 @@ async function autotypingCommand(sock, chatId, message) {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363161513685998@newsletter',
-                            newsletterName: 'KnightBot MD',
+                            newsletterJid: '120363406449026172@newsletter',
+                            newsletterName: 'DexBot MD',
                             serverMessageId: -1
                         }
                     }
@@ -85,8 +85,8 @@ async function autotypingCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363406449026172@newsletter',
+                    newsletterName: 'DexBot MD',
                     serverMessageId: -1
                 }
             }
@@ -100,8 +100,8 @@ async function autotypingCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363406449026172@newsletter',
+                    newsletterName: 'DexBot MD',
                     serverMessageId: -1
                 }
             }
